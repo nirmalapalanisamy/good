6588a1056b6ea274e8b31033973bbd2e23bb68eb
nirmalapalanisamy <nirmalakalai08@gmail.com>
2016-08-07 11:49:53 +0530

    Add files via upload

---
+++ subset.java
@@ -0,0 +1,13 @@
+public static boolean contains(int[] set1, int[] set2) {
+    boolean contains = false;
+    for (int i = 0; i < set1.length; i++) {
+        for (int a = 0; a < set2.length - 1; a++) {
+            if (set1[i] == set2[a] && set1[i + 1] == set2[a + 1]) {
+                contains = true;
+            } else {
+                contains = false;
+            }
+        }
+    }
+    return contains;
+}
